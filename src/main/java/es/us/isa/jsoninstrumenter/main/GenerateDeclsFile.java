5b01093f6bd404c5533db7aed52e24a42095dea6
Juan Carlos Alonso Valenzuela <44436290+JuanCarlosAlonsoValenzuela@users.noreply.github.com>
2022-04-19 21:27:07 +0100

    OMDb files added

--- src/main/java/es/us/isa/jsoninstrumenter/main/GenerateDeclsFile.java
+++ src/main/java/es/us/isa/jsoninstrumenter/main/GenerateDeclsFile.java
@@ -24,9 +24,9 @@ import static es.us.isa.jsoninstrumenter.util.TestCaseFileManager.getTestCasesFr
 
 public class GenerateDeclsFile {
 
-    private static String openApiSpecPath = "src/test/resources/evaluation/AmadeusHotel/swagger.yaml";
-    private static String testCasesFilePath = "src/test/resources/evaluation/GitHub/GitHub2_extended.csv";
-    private static boolean generateDtrace = false;
+    private static String openApiSpecPath = "src/test/resources/evaluation/OMDb/bySearch/swagger_bySearch.yaml";
+    private static String testCasesFilePath = "src/test/resources/evaluation/OMDb/bySearch/OMDb_bySearch_1000.csv";
+    private static boolean generateDtrace = true;
 
 //    cd /mnt/d/users/jcav/Documents/GitHub/json-instrumenter/src/main/resources
 //    cd /mnt/d/users/Juan\ Carlos/Documents/GitHub/json-instrumenter/src/main/resources
